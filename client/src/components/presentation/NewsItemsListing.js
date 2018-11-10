6b6441618780ee8406ad4ec75ce80141a8f5a8bd
Eva Lee <slee2@brookdale.com>
2018-11-10 00:16:12 -0600

    more practice 2

---
+++ client/src/components/presentation/NewsItemsListing.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class NewsItemsListing extends Component {
+    render() {
+        return (
+          <div>
+                <div><Link to={`/news/${this.props.data.id}`}><b>{this.props.data.title}</b></Link></div>
+                <div>{this.props.data.teaser}</div>
+          </div>
+        )
+    }
+}
+
+NewsItemListing.propTypes = {
+    data: PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        title: PropTypes.string.isRequired,
+        teaser: PropTypes.string.isRequired
+})
+};
+
+export default NewsItemsListing
